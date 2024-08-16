12737b41eb8d2d3a56229ee9c84cd8a0d602c1b5
CHRISTABEL <obinwosugosiorah@gmail.com>
2024-08-16 18:31:39 -0500

    updated

---
+++ src/component/Quiz.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Quiz.css";
+const Quiz = () => {
+  return (
+    <div className="quiz">
+      <div className="questions">
+        Which is the best software company in Nigeria?
+      </div>
+      <div className="anwsers">
+        <div className="anwser"> OGTech networks LTD</div>
+        <div className="anwser"> OGTech networks LTD</div>
+        <div className="anwser"> OGTech networks LTD</div>
+        <div className="anwser"> OGTech networks LTD</div>
+      </div>
+    </div>
+  );
+};
+
+export default Quiz;
