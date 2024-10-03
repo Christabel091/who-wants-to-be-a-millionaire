7731027f4095e163f45f0ebfbb6ac8a5275c7c00
CHRISTABEL <obinwosugosiorah@gmail.com>
2024-10-02 23:10:02 -0500

    created a sample data

---
+++ src/data.js
@@ -0,0 +1,114 @@
+const data = [
+  {
+    id: 1,
+    question: "Who is the first black president of America?",
+    answers: [
+      {
+        text: "Goodluck Jonathan",
+        correct: false,
+      },
+      {
+        text: "Barack Obama",
+        correct: true,
+      },
+      {
+        text: "Donald Trump",
+        correct: false,
+      },
+      {
+        text: "Joe Biden",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "Rolex is a company that specializes in what kind of product?",
+    answers: [
+      {
+        text: "Phone",
+        correct: false,
+      },
+      {
+        text: "Food",
+        correct: false,
+      },
+      {
+        text: "Cosmetics",
+        correct: false,
+      },
+      {
+        text: "Watch",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "When did the Facebook website launch?",
+    answers: [
+      {
+        text: "2004",
+        correct: true,
+      },
+      {
+        text: "2000",
+        correct: false,
+      },
+      {
+        text: "2020",
+        correct: false,
+      },
+      {
+        text: "2999",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "What is the top software company in Nigeria?",
+    answers: [
+      {
+        text: "Soft Space Ltd",
+        correct: false,
+      },
+      {
+        text: "Facebook",
+        correct: false,
+      },
+      {
+        text: "OGTech Networks LTD",
+        correct: true,
+      },
+      {
+        text: "Nigeria Coders Academy",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Who played the role of 'Harry' in the movie Harry Potter?",
+    answers: [
+      {
+        text: "Gabriel God'spower",
+        correct: false,
+      },
+      {
+        text: "Leonardo DiCaprio",
+        correct: false,
+      },
+      {
+        text: "Denzel Washington",
+        correct: false,
+      },
+      {
+        text: "Daniel Radcliffe",
+        correct: true,
+      },
+    ],
+  },
+];
+
+export default data;
